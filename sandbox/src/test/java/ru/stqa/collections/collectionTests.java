e62352a7238876730f63c7af92c61968e609250f
Aleksandra_Nemchenko <mara-alexa@mail.ru>
2024-01-21 22:50:44 +0300

    Коллекции, массивы

---
+++ sandbox/src/test/java/ru/stqa/collections/collectionTests.java
@@ -0,0 +1,38 @@
+package ru.stqa.collections;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class collectionTests {
+
+    @Test
+    void arrayTests(){
+        //var array = new String[]{"a","b","c"};
+        var array = new String[3];
+        array[0] = "a";
+        Assertions.assertEquals(3, array.length);
+        Assertions.assertEquals("a", array [0]);
+
+        array[0] = "d";
+        Assertions.assertEquals("d", array [0]);
+    }
+
+    @Test
+    void listTests(){
+        //var list = List.of("a","b","c");
+        var list = new ArrayList<>(List.of("a","b","c"));
+        Assertions.assertEquals(3, list.size());
+
+       // list.add("a");list.add("b");
+       // list.add("c");
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals("a", list.get(0));
+
+        list.set(0,"d");
+        Assertions.assertEquals("d", list.get(0));
+    }
+}
