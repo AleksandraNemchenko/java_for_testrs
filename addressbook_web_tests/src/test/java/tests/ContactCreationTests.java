d1f8d5330645354237455bdee3ecf1a0013c8837
Aleksandra_Nemchenko <mara-alexa@mail.ru>
2024-01-24 23:25:05 +0300

    Параметризованные тесты для контактов

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -2,57 +2,54 @@ package tests;
 
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
-    @Test
-    public void canCreateContact() {
-        int groupCount =  app.contact().getCount();
-        app.contact().createContact(new ContactData("First name", "Last name", "Address", "Mobile", "E-mail"));
-        int newGroupCount =  app.contact().getCount();
-        Assertions.assertEquals(groupCount + 1, newGroupCount);
-    }
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
+        for (var firstName : List.of("", "First name")) {
+            for (var lastName : List.of("", "Last name")) {
+                for (var address : List.of("", "Address")) {
+                    for (var mobile : List.of("", "Mobile")) {
+                        for (var email : List.of("", "E-mail")) {
+                            result.add(new ContactData(firstName, lastName, address, mobile, email));
+                        }
+                    }
+                }
+            }
 
-    @Test
-    public void canCreateEmptyContact() {
-        app.contact().createContact(new ContactData());
-    }
-
-    @Test
-    public void canCreateContactWithFirstNameOnly() {
-        app.contact().createContact(new ContactData().withFirstName("First name"));
-    }
-
-    @Test
-    public void canCreateContactWithLastNameOnly() {
-        app.contact().createContact(new ContactData().withLastName("Last name"));
-    }
-
-    @Test
-    public void canCreateContactWithAddressOnly() {
-        app.contact().createContact(new ContactData().withAddress("Address"));
-    }
-
-    @Test
-    public void canCreateContactWithMobileOnly() {
-        app.contact().createContact(new ContactData().withMobile("Mobile"));
-    }
-
-    @Test
-    public void canCreateContactWithEmailOnly() {
-        app.contact().createContact(new ContactData().withEmail("E-mail"));
-    }
-
-    @Test
-    public void canCreateMultipleContacts() {
-        int n = 5;
-        int groupCount =  app.contact().getCount();
-
-        for (int i = 0; i < n; i++){
-            app.contact().createContact(new ContactData(randomString(i), "Last name", "Address", "Mobile", "E-mail"));
         }
+        for (int i = 0; i < 5; i++) {
+            result.add(new ContactData(randomString(i * 10), randomString(i * 10), randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
 
-        int newGroupCount =  app.contact().getCount();
-        Assertions.assertEquals(groupCount + n, newGroupCount);
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContacts(ContactData contact) {
+        int contactCount = app.contact().getCount();
+        app.contact().createContact(contact);
+        int newContactCount = app.contact().getCount();
+        Assertions.assertEquals(contactCount + 1, newContactCount);
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeContactProvider")
+    public void canNotCreateContact(ContactData contact) {
+        int contactCount = app.contact().getCount();
+        app.contact().createContact(contact);
+        int newContactCount = app.contact().getCount();
+        Assertions.assertEquals(contactCount, newContactCount);
+    }
+
+    public static List<ContactData> negativeContactProvider() {
+        var result = new ArrayList<ContactData>(List.of(
+                new ContactData("contact name '", "", "", "", "")));
+        return result;
     }
 }
