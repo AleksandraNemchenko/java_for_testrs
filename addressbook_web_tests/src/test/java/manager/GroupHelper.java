eee8c363e2c400f52edcf2da21dbdce4fc606e01
Aleksandra_Nemchenko <mara-alexa@mail.ru>
2024-01-31 16:06:51 +0300

    Сравнение списков

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -3,6 +3,9 @@ package manager;
 import model.GroupData;
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupHelper extends HelperBase{
 
     public GroupHelper (ApplicationManager manager) {
@@ -21,15 +24,15 @@ public class GroupHelper extends HelperBase{
         submitGroupCreation();
         returnToGroupsPage();
     }
-    public void removeGroup() {
+    public void removeGroup(GroupData group) {
         openGroupPage();
-        selectGroup();
+        selectGroup(group);
         removeSelectedGroups();
         returnToGroupsPage();
     }
     public void modifyGroup(GroupData modifiedGroup) {
         openGroupPage();
-        selectGroup();
+        selectGroup(null);
         initGroupModification();
         fillGroupForm(modifiedGroup);
         submitGroupModification();
@@ -60,8 +63,8 @@ public class GroupHelper extends HelperBase{
         type(By.name("group_header"), group.header());
         type(By.name("group_footer"), group.footer());
     }
-    private void selectGroup() {
-        click(By.name("selected[]"));
+    private void selectGroup(GroupData group) {
+        click(By.cssSelector(String.format("input[value='%s']", group.id())));
     }
     private void initGroupModification() {
         click(By.name("edit"));
@@ -83,5 +86,17 @@ public class GroupHelper extends HelperBase{
         for (var checkbox : checkboxes){
             checkbox.click();
         }
+    }
+
+    public List<GroupData> getList(){
+        var groups = new ArrayList<GroupData>();
+        var spans = manager.driver.findElements(By.cssSelector("span.group"));
+        for (var span : spans){
+            var name = span.getText();
+            var checkbox = span.findElement(By.name("selected[]"));
+            var id = checkbox.getAttribute("value");
+            groups.add(new GroupData().withId(id).withName(name));
+        }
+        return groups;
     }
 }
