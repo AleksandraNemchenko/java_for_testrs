13ca10703b4c934120ad64fcc8ad18c6dc7c89ed
Aleksandra_Nemchenko <mara-alexa@mail.ru>
2024-02-09 20:30:59 +0300

    Создание генератора тестовых данных для групп

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTests.java
@@ -1,7 +1,7 @@
-package tests;
+package ru.stqa.addressbook.tests;
 
-import model.ContactData;
-import model.GroupData;
+import ru.stqa.addressbook.common.CommonFunctions;
+import ru.stqa.addressbook.model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -33,11 +33,11 @@ public class ContactCreationTests extends TestBase {
         }
         for (int i = 0; i < 5; i++) {
             result.add(new ContactData()
-                    .withFirstName(randomString(i * 10))
-                    .withLastName(randomString(i * 10))
-                    .withAddress(randomString(i * 10))
-                    .withMobile(randomString(i * 10))
-                    .withEmail(randomString(i * 10))
+                    .withFirstName(CommonFunctions.randomString(i * 10))
+                    .withLastName(CommonFunctions.randomString(i * 10))
+                    .withAddress(CommonFunctions.randomString(i * 10))
+                    .withMobile(CommonFunctions.randomString(i * 10))
+                    .withEmail(CommonFunctions.randomString(i * 10))
                     .withPhoto(randomFile ("src/test/resources/images")));
         }
         return result;
