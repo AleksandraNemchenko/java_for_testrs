23eea48df9c758a90cf00b7c10a9a122f495e912
Aleksandra_Nemchenko <mara-alexa@mail.ru>
2024-02-23 19:28:25 +0300

    Функциональный стиль программирования

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -2,29 +2,36 @@ package ru.stqa.geometry;
 
 import ru.stqa.geometry.figures.*;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+
 public class Geometry {
     public static void main(String[] args) {
 
-        Square.printSquareArea (new Square(7.0));
-        Square.printSquareArea (new Square(5.0));
-        Square.printSquareArea (new Square(3.0));
+        var squares = List.of(new Square(7.0),new Square(5.0), new Square(3.0));
+//        for (Square square : squares) {
+//            Square.printSquareArea(square);
+//        }
 
-        Rectangle.printRectaglArea (3.0, 2.0);
-        Rectangle.printRectaglArea (7.0, 9.0);
+        squares.forEach(Square::printSquareArea);
 
-        Triangle t = new Triangle(8.0, 10.0, 6.0);
-        System.out.println(t.perimeter());
-        System.out.println(t.square());
-
-        Triangle t2 = new Triangle (6.0, 6.0, 4.0);
-        System.out.println(t2.perimeter());
-        System.out.println(t2.square());
-
-        Circle c = new Circle(5.0);
-        System.out.println(c.areaCircle());
-
-        RightTriangle d = new RightTriangle (4.0, 3.0);
-        System.out.println(d.AreaRightTriangle());
+//        Rectangle.printRectaglArea (3.0, 2.0);
+//        Rectangle.printRectaglArea (7.0, 9.0);
+//
+//        Triangle t = new Triangle(8.0, 10.0, 6.0);
+//        System.out.println(t.perimeter());
+//        System.out.println(t.square());
+//
+//        Triangle t2 = new Triangle (6.0, 6.0, 4.0);
+//        System.out.println(t2.perimeter());
+//        System.out.println(t2.square());
+//
+//        Circle c = new Circle(5.0);
+//        System.out.println(c.areaCircle());
+//
+//        RightTriangle d = new RightTriangle (4.0, 3.0);
+//        System.out.println(d.AreaRightTriangle());
 
     }
 }
